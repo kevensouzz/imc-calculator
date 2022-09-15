c736ac538c207aee37cfb480cf773363249a97bb
kevensouzz <kevenssouza1@gmail.com>
2022-09-15 11:33:21 -0300

    Update Files

--- js/script.js
+++ js/script.js
@@ -1,3 +1,25 @@
 function calcular() {
-    
+    let height = document.getElementById('campo-altura').value / 100
+
+    let weight = document.getElementById('campo-peso').value
+
+    let imc = weight / height ** 2
+
+    document.getElementById('campo-resultado-imc').innerHTML = imc
+
+    if (imc < 18.5) {
+        document.getElementById('campo-resultado-text').innerHTML = 'ABAIXO DO PESO IDEAL!'
+    }
+
+    else if (imc < 24.9) {
+        document.getElementById('campo-resultado-text').innerHTML = 'PESO IDEAL!'
+    }
+
+    else if (imc < 39.9) {
+        document.getElementById('campo-resultado-text').innerHTML = 'ACIMA DO PESO IDEAL!'
+    }
+
+    else if (imc > 39.9) {
+        document.getElementById('campo-resultado-text').innerHTML = 'MUITO ACIMA DO PESO IDEAL!'
+    }
 }
